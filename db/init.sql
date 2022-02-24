d4498f28ea6dd682566dbd60f4f897cfcbd48bdb
Daniel Grønbjerg <danielgronbjerg@gmail.com>
2022-02-24 08:49:24 +0100

    Add fields

--- db/init.sql
+++ db/init.sql
@@ -8,9 +8,20 @@ CREATE TABLE IF NOT EXISTS login_info(
    salt varchar(32) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS address(
+   id SERIAL PRIMARY KEY,
+   streetname varchar(64) NOT NULL,
+   streetnumber varchar(8) NOT NULL,
+   zipcode varchar(32) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS pharmacy(
    id SERIAL PRIMARY KEY,
-   pharmacy_name varchar(64) NOT NULL
+   pharmacy_name varchar(64) NOT NULL,
+   address_id bigint,
+   CONSTRAINT fk_address
+      FOREIGN KEY(address_id) 
+	  REFERENCES address(id)
 );
 
 CREATE TABLE IF NOT EXISTS user_role(
@@ -25,7 +36,15 @@ CREATE TABLE IF NOT EXISTS user_permission(
 CREATE TABLE IF NOT EXISTS person(
    id SERIAL PRIMARY KEY,
    first_name varchar(64) NOT NULL,
-   last_name varchar(64) NOT NULL
+   last_name varchar(64) NOT NULL,
+   login_id bigint,
+   role_id bigint,
+   CONSTRAINT fk_login
+      FOREIGN KEY(login_id) 
+	  REFERENCES login_info(id),
+   CONSTRAINT fk_role
+      FOREIGN KEY(role_id) 
+	  REFERENCES user_role(id)
 );
 
 CREATE TABLE IF NOT EXISTS patient(
@@ -39,11 +58,11 @@ CREATE TABLE IF NOT EXISTS pharmaceut(
    CONSTRAINT fk_pharmacy
       FOREIGN KEY(pharmacy_id) 
 	  REFERENCES pharmacy(id)
-);
+) INHERITS (person);
 
 CREATE TABLE IF NOT EXISTS doctor(
    id SERIAL PRIMARY KEY
-);
+) INHERITS (person);
 
 CREATE TABLE IF NOT EXISTS medicine(
    id SERIAL PRIMARY KEY,
@@ -53,7 +72,19 @@ CREATE TABLE IF NOT EXISTS medicine(
 CREATE TABLE IF NOT EXISTS prescription(
    id SERIAL PRIMARY KEY,
    expiration DATE,
-   creation TIMESTAMP
+   creation TIMESTAMP NOT NULL,
+   medicine_id bigint NOT NULL,
+   prescribed_by bigint NOT NULL,
+   last_administered_by bigint NOT NULL,
+   CONSTRAINT fk_medicine
+      FOREIGN KEY(medicine_id) 
+	   REFERENCES medicine(id),
+   CONSTRAINT fk_prescriber
+      FOREIGN KEY(prescribed_by) 
+	   REFERENCES doctor(id),
+   CONSTRAINT fk_administered_by
+      FOREIGN KEY(last_administered_by) 
+	   REFERENCES pharmaceut(id)
 );
 
 
