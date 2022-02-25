e94be5a6c4899171a53ba5a51eeb7508cf5169df
Benjo6 <58556176+Benjo6@users.noreply.github.com>
2022-02-25 16:13:00 +0100

    EmailService WORKS!!!!

---
+++ Pharmacy-Assignment2/Pharmacy-Assignment2/Controllers/Apis/EmailController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pharmacy_Assignment2.Interfaces;
+using Pharmacy_Assignment2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmacy_Assignment2.Controllers.Apis
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IMailService _mailService;
+        public EmailController(IMailService mailService)
+        {
+            _mailService = mailService;
+
+        }
+
+
+        [HttpPost("Send")]
+        public async Task<IActionResult> Send([FromForm] MailRequest mailRequest)
+        {
+            try
+            {
+                await _mailService.SendEmailAsync(mailRequest);
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Pharmacy-Assignment2/Pharmacy-Assignment2/Interfaces/IMailService.cs
@@ -0,0 +1,13 @@
+﻿using Pharmacy_Assignment2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmacy_Assignment2.Interfaces
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(MailRequest mailRequest);
+    }
+}
