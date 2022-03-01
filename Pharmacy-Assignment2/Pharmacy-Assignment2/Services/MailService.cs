18447475fd8e0975f2598c2c0ee6ed3228f1094b
Daniel Grønbjerg <danielgronbjerg@gmail.com>
2022-03-01 19:20:35 +0100

    change to test email

--- Pharmacy-Assignment2/Pharmacy-Assignment2/Services/MailService.cs
+++ Pharmacy-Assignment2/Pharmacy-Assignment2/Services/MailService.cs
@@ -1,5 +1,6 @@
 ﻿using MailKit.Net.Smtp;
 using MailKit.Security;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MimeKit;
 using Pharmacy_Assignment2.Interfaces;
@@ -16,13 +17,16 @@ namespace Pharmacy_Assignment2.Services
     public class MailService : IMailService
     {
         private readonly MailSettings _mailSettings;
+        private readonly ILogger<MailService> _logger;
 
-        public MailService(IOptions<MailSettings> options)
+        public MailService(IOptions<MailSettings> options, ILogger<MailService> logger)
         {
             _mailSettings = options.Value;
+            _logger = logger;
         }
         public async Task SendEmailAsync(MailRequest mailRequest)
         {
+            _logger.LogInformation($"Sending mail to {mailRequest.ToEmail}");
             var email = new MimeMessage();
             email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
             email.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
@@ -52,6 +56,7 @@ namespace Pharmacy_Assignment2.Services
             smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
             await smtp.SendAsync(email);
             smtp.Disconnect(true);
+            _logger.LogInformation($"Done sending mail to {mailRequest.ToEmail}");
         }
     }
 }
